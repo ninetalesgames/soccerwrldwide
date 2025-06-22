3464f24adb6ada9c033be8f93b4b969e70b726be
ninetalesgames <ninetales@ninetalesgames.co.uk>
2025-06-22 22:23:13 +0100

    Add 404 fallback and redirect support for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+const redirect = sessionStorage.redirect;
+if (redirect) {
+  sessionStorage.removeItem('redirect');
+  window.history.replaceState(null, '', redirect);
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
